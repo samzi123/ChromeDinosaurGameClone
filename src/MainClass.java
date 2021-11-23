d9c4c06be57b8bd8ab75ba296dcbfefea3891c72
Moez Boussarsar <moezboussarsar@dhcp-10-5-54-163.wireless.rochester.edu>
2021-11-22 19:52:51 -0500

    testing again

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,8 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-System.out.println("testingg");
+		System.out.println("testingg");
+		System.out.println("testing again");
 	}
 
 }
